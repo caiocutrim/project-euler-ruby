044aa0fce6c870ef98d233f3847b61a6dd753b45
Jeff Johnson <jeffjohnson9046hotmail.com>
2013-05-09 11:59:05 -0700

    commented out code that isn't being called... reduced the not-learning-anything answer to 1 line.

--- problem5.rb
+++ problem5.rb
@@ -1,24 +1,21 @@
 # A VERY basic implementation of the Euclidian GCD algorithm:
 # http://en.wikipedia.org/wiki/Euclidean_algorithm
-def gcd(a, b)
-	while b != 0 # keep dividing while there is a remainder
-		remainder = a % b
-		a = b
-		b = remainder
-	end
+ # def gcd_jj(a, b)
+ # 	while b != 0 # keep dividing while there is a remainder
+ # 		remainder = a % b
+ # 		a = b
+ # 		b = remainder
+ # 	end
+ # end
 
-	return a
-end
-
-# Got the equation from http://rosettacode.org/wiki/Least_common_multiple,
-# which was pretty simple to convert.
-def lcm(a, b)
-	a * b / gcd(a, b)
-end
+# # Got the equation from http://rosettacode.org/wiki/Least_common_multiple,
+# # which was pretty simple to convert.
+ # def lcm_jj(a, b)
+ # 	a * b / gcd_jj(a, b)
+ # end
 
 # Quit your jibber-jabber and run it!
-nums = (1..20).to_a
-puts nums.reduce(:lcm)
+puts (1..20).to_a.reduce(:lcm)
 
 # puts "GCD of 2 and 4: #{gcd(2, 4)}"
 # puts "GCD of 9 and 15: #{gcd(3, 15)}"
