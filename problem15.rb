01b38738a7ae65470f05293a31704aea4dd8c65b
Jeff Johnson <jeffjohnson9046hotmail.com>
2013-07-25 16:43:41 -0700

    tiny change to factorial method to make it more ruby-like.

--- problem15.rb
+++ problem15.rb
@@ -6,7 +6,7 @@
 ###############################################################################
 def factorial(n)
 	return 1 if n == 1
-	return n * factorial(n - 1)
+	n * factorial(n - 1)
 end
 
 start_time = Time.now
